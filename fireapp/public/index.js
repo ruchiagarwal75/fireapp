4cd35b9ff7c3f7cfa2449f7fb9175ee9a2722aaf
ruchiagarwal75 <ruchiii75@gmail.com>
2016-11-18 12:34:24 -0800

    initial setup for application

---
+++ fireapp/public/index.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.6.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDTqRmCtAetPMl0bJ0EUuFgNXfNe3sR2PQ",
+    authDomain: "fireapp-32341.firebaseapp.com",
+    databaseURL: "https://fireapp-32341.firebaseio.com",
+    storageBucket: "fireapp-32341.appspot.com",
+    messagingSenderId: "151799374085"
+  };
+  firebase.initializeApp(config);
+</script>
